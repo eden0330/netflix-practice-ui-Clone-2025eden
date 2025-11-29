d62757948f3ac67fd5f567d12aaaf714a6c5b69c
eden0330 <edadani1415@gmail.com>
2025-11-29 18:51:44 +0300

    add home

--- netflix-clone/src/Page/Home/Home.jsx
+++ netflix-clone/src/Page/Home/Home.jsx
@@ -3,14 +3,13 @@ import Header from '../../components/Header/Header'
 import Footer from '../../components/Footer/Footer';
 import Banner from '../../components/Banner/Banner';
 import RowList from '../../components/Rows/RowList';
-import Hea from '../../components/HEA/Hea';
+
 
 
 const Home = () => {
   return (
     <div>
       <Header /><br></br>
-      {/* <Hea /> */}
 <Banner />
 <RowList />
       <Footer />
